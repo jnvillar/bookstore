a48cb6045dee168c62d1358e978ab0ba36e3ed28
Juan Noli <juan.villar@wildlifestudios.com>
2021-02-22 23:48:27 -0300

    heroku

--- server/config/app.go
+++ server/config/app.go
@@ -1,11 +1,25 @@
 package config
 
+import (
+	"os"
+	"strconv"
+)
+
 type AppConfig struct {
 	Port int
 }
 
+func getPort() int {
+	port := os.Getenv("port")
+	if port == "" {
+		return 8080
+	}
+	p, _ := strconv.Atoi(port)
+	return p
+}
+
 func devAppConfig() *AppConfig {
 	return &AppConfig{
-		Port: 8080,
+		Port: getPort(),
 	}
 }
